22fc97b1e6d2422c00e5ec3330ed75acee1d8786
akuryou <akuryou@byteweaver.org>
2016-02-10 13:48:12 +0100

    add minimal entry file

---
+++ src/entry.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+
+  require('node_modules/reveal.js/css/reveal.css');
+  require('node_modules/reveal.js/css/theme/blood.css');
+
+  var Reveal = require('reveal.js');
+  Reveal.initialize();
+
+})();
