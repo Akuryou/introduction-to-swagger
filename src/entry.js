b39b2fdb63c9beb2374f177ca0fd63297d1b85d7
akuryou <akuryou@byteweaver.org>
2016-02-11 09:50:56 +0100

    remove empty line

--- src/entry.js
+++ src/entry.js
@@ -4,7 +4,6 @@
   require('node_modules/reveal.js/css/reveal.css');
   require('node_modules/reveal.js/css/theme/blood.css');
   require('node_modules/font-awesome/css/font-awesome.css');
-
   require('node_modules/highlight.js/styles/monokai-sublime.css');
 
   window.hljs = require('highlight.js');
