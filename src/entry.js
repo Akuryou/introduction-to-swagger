5ab3c42035f3cae4334dbdb3a332a696ad5a960c
akuryou <akuryou@byteweaver.org>
2016-02-11 08:44:12 +0100

    add font-awesome support

--- src/entry.js
+++ src/entry.js
@@ -3,6 +3,7 @@
 
   require('node_modules/reveal.js/css/reveal.css');
   require('node_modules/reveal.js/css/theme/blood.css');
+  require('node_modules/font-awesome/css/font-awesome.css');
 
   var Reveal = require('reveal.js');
   Reveal.initialize();
