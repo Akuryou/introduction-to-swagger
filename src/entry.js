29bee7d8d20f723da0a5219992625e58701c1b5e
akuryou <akuryou@byteweaver.org>
2016-02-11 09:01:53 +0100

    add highlight.js

--- src/entry.js
+++ src/entry.js
@@ -5,10 +5,14 @@
   require('node_modules/reveal.js/css/theme/blood.css');
   require('node_modules/font-awesome/css/font-awesome.css');
 
+  require('node_modules/highlight.js/styles/monokai-sublime.css');
+
+  window.hljs = require('highlight.js');
   window.Reveal = require('reveal.js');
 
   setTimeout(function() {
     window.Reveal.initialize({});
+    hljs.initHighlighting();
   }, 1000);
 
 })();
